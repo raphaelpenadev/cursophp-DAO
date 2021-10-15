15e004ecc2ecac87543f26e5462342e27ca486cf
Raphael Pena <raphaelnrk@gmail.com>
2021-10-14 22:45:46 -0300

    Criação do DAO para buscar um usuario, todos os usuarios, usuarios pelas iniciais ou finais passadas e carregar dados com login e senha

--- index.php
+++ index.php
@@ -2,8 +2,22 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+// Carrega um usuario (toString)
+// $root = new Usuario();
+// $root->LoadById(4);
+// echo $root;
 
-$root->LoadById(4);
+// Carrega uma lista de usuarios (toList)
+// $lista = Usuario::getList();
 
-echo $root;
+// echo json_encode($lista);
+
+// Carrega uma lista de usuarios buscando pelas letras passadas
+// $search = Usuario::search("jo");
+
+// echo json_encode($search);
+
+// Carrega Usuario completando login e senha
+$usuario = new Usuario();
+$usuario->login('jose', '12345');
+echo $usuario;
